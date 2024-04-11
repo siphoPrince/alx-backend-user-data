6aa89dc1b42c394c9a8ae21149f232f33ab2b1a1
siphoprince <mabirimisesipho@gmail.com>
2024-04-11 08:35:28 -0700

    user data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,26 @@
 
 
 import re
+import logging
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
 
-import re
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, sensitive_data=()):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.sensitive_data = sensitive_data
+
+    def format(self, record: logging.LogRecord) -> str:
+        # Compile sensitive data pattern for efficiency
+        pattern = re.compile(fr"(?:"+ self.SEPARATOR + r"(?:\w+={0,1})(" + r"|".join(self.sensitive_data) + r"))=([^;]+)")
+        # Apply redaction using pattern substitution
+        redacted_msg = pattern.sub(rf"\1={self.REDACTION}", record.msg)
+        record.msg = redacted_msg
+        return super().format(record)
 
 def filter_datum(fields, redaction, message, separator):
     pattern = r"(?:"+ separator + r"(?:\w+={0,1})(" + r"|".join(fields) + r"))=([^;]+)"
