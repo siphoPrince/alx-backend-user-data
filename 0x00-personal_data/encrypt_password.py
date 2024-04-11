2f72e9a8ddb8c2418695c839f24dd4b9f41f1050
siphoprince <mabirimisesipho@gmail.com>
2024-04-11 11:02:31 -0700

    check pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,9 +5,16 @@ Main file demonstrating secure password hashing with bcrypt.
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt with random salt.
     """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """provided password matches the stored hashed password using bcrypt.
+    """
+   return bcrypt.checkpw(password.encode(), hashed_password)
