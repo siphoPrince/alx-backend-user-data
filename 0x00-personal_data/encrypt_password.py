500fe728c6005c88117a2e5ea780a0ce3def64f7
siphoprince <mabirimisesipho@gmail.com>
2024-04-13 01:58:02 -0700

    pycode

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,14 +15,7 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
-def is_valid(hashed_password: bytes, password: str) -> bool:
-    """
-    Validates that the provided password matches the hashed password
-    """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
 
 if __name__ == "__main__":
     password = "MyAmazingPassw0rd"
-    encrypted_password = hash_password(password)
     print(hash_password(password).decode('utf-8'))
