6caaed0b224c2f868c8484ba5a8785e7a1df06b3
siphoprince <mabirimisesipho@gmail.com>
2024-04-16 06:44:11 -0700

    index and app fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,8 +2,8 @@
 """
 Route module for the API
 """
+
 from os import getenv
-from api.v1.views.index import index_blueprint
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -12,16 +12,16 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.register_blueprint(index_blueprint)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
 
-@app.errorhandler(401)
-def unauthorized_error_handler(error):
-    """unauth"""
-    response = jsonify({"error": "Unauthorized"})
-    response.status_code = 401
-    return response
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -29,6 +29,37 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> str:
+    """ Filter each request
+    """
+    request_path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
+
+    if auth:
+        if auth.require_auth(request.path, request_path_list):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,16 +5,6 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-index_blueprint = Blueprint("index", __name__)
-
-
-
-@index_blueprint.route("/api/v1/unauthorized")
-def unauthorized_endpoint():
-    """un auth"""
-    abort(401)
-
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -34,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error
+    """
+    abort(403)
