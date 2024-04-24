66b87c70e7b60418f2d5c7b00d2d0ffad40bdc81
siphoprince <mabirimisesipho@gmail.com>
2024-04-24 08:17:50 -0700

    calllinfg functions

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""main file for database"""
 
+""" Database class to save and update databse
+"""
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,20 +11,19 @@ from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
-class Database:
-    """A class to interact with the database
-    """
+class DB:
+    """ main data base class """
 
     def __init__(self):
-        """Initialize the database
-        """
-        self._engine = create_engine("sqlite:///users.db", echo=False)
+        """ Init function """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
-        """Get a session object
+        """ returns a session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -31,19 +31,23 @@ class Database:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-        """
+        """ adding database """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user by specified criteria
-        """
-        allowed_keys = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+        """input argument to find user """
+        user_keys = [
+            'id',
+            'email',
+            'hashed_password',
+            'session_id',
+            'reset_token']
+
         for key in kwargs.keys():
-            if key not in allowed_keys:
+            if key not in user_keys:
                 raise InvalidRequestError
         result = self._session.query(User).filter_by(**kwargs).first()
         if result is None:
@@ -51,12 +55,18 @@ class Database:
         return result
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Update user attributes in the database
-        """
+        """ updates to the database """
         user_to_update = self.find_user_by(id=user_id)
-        allowed_keys = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+
+        user_keys = [
+            'id',
+            'email',
+            'hashed_password',
+            'session_id',
+            'reset_token']
+
         for key, value in kwargs.items():
-            if key in allowed_keys:
+            if key in user_keys:
                 setattr(user_to_update, key, value)
             else:
                 raise ValueError
