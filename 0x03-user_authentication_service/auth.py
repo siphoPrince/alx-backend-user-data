b47b9d452f04e74cd484afd970af31696e85345c
siphoprince <mabirimisesipho@gmail.com>
2024-04-25 13:20:30 -0700

    auth pycode fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,7 +13,6 @@ from db import DB
 from user import User
 
 
-
 def _hash_password(password: str) -> str:
     """ Generate a salted hash of the password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
